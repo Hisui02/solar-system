c1fda6cf60845633f296e7b69efe6271fb381f3c
Hisui <alvarolgleiva@gmail.com>
2023-12-11 14:46:58 +0100

    terminado

--- js/menu-hamburguesa.js
+++ js/menu-hamburguesa.js
@@ -1,4 +1,7 @@
 function toggleMenu() {
   const nav = document.querySelector("nav");
   nav.classList.toggle("active");
+  document.querySelector("nav div").addEventListener("click", function () {
+    document.querySelector("nav").classList.remove("active");
+  });
 }
